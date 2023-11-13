7396225a3fd5c2bef70d71675c26502e72060645
Samuele Serri <samueleserri@outlook.it>
2023-11-13 16:30:02 +0100

    insertion sort added

--- insertionsort.py
+++ insertionsort.py
@@ -1,1 +1,9 @@
+def insertion_sort(A):
+    for j in range(1, len(A), 1):
+        key = A[j]
+        i = j-1
+        while i >= 0 and A[i]>key:
+            A[i+1]=A[i]
+            i -=1
+            A[i+1]= key
 
